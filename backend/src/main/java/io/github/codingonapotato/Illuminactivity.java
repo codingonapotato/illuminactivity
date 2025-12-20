6d5a6aefba689af733e266176efe2aace5e7ae00
codingonapotato <hcpc215@gmail.com>
2025-12-20 14:09:00 -0800

    refactor: rename Main class

--- backend/src/main/java/io/github/codingonapotato/Illuminactivity.java
+++ backend/src/main/java/io/github/codingonapotato/Illuminactivity.java
@@ -1,6 +1,6 @@
 package io.github.codingonapotato;
 
-public class Main {
+public class Illuminactivity {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
