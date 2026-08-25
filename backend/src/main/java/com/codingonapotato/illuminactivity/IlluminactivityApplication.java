fd46f4d6a7fd17fcd65477f01006f7a881ac484c
codingonapotato <hc2020@student.ubc.ca>
2026-08-25 11:55:24 -0700

    add spring boot initializer starter files

---
+++ backend/src/main/java/com/codingonapotato/illuminactivity/IlluminactivityApplication.java
@@ -0,0 +1,13 @@
+package com.codingonapotato.illuminactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlluminactivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlluminactivityApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codingonapotato/illuminactivity/IlluminactivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingonapotato.illuminactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlluminactivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
